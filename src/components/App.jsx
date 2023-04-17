4da6cd640e2c2c1e0347da324e35a516ea265fd8
Unclemortuary <artem_mihalych@mail.ru>
2023-04-17 22:01:27 +0300

    project configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <h1 className="text-4xl text-blue-700">Merhaba!</h1>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = function(env, argv) {
+  return {
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+      path: path.join(__dirname, "/build"),
+      filename: "index.js",
+      clean: true,
+      library: {
+        name: 'github-viewer',
+        type: 'umd'
+      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "src/index.html",
+        })
+    ],
+    devServer: {
+        port: 3030,
+        watchFiles: 'src/*',
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+    },
+    devtool: 'eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'src'),
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          type: 'asset/inline'
+        },
+      ],
+    },
+    resolve: {
+      enforceExtension: false,
+      extensions: ['.js', '.jsx', '.scss', '.html', '.json'],
+    },
+    optimization: {
+      minimize: true
+    }   
+  }
+};
